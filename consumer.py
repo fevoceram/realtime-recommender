1408caa1cc26d2138eb18ccf05793b5eb70f5ba2
Hammad Idrees <fevoceram192@gmail.com>
2026-04-20 13:24:21 -0400

    consumer.py

---
+++ consumer.py
@@ -0,0 +1,11 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer(
+    'user_events',
+    bootstrap_servers='localhost:9092',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
+for message in consumer:
+    print("Consumed:", message.value)
